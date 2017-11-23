8dbfdc26ca0fc5f3c5f02e6008b49fa911bf7417
Peter Sumbler <peter@gavagai.se>
2017-11-23 15:47:00 +0100

    Edge case refactoring (term not in resource)

--- morphological_expander.py
+++ morphological_expander.py
@@ -8,7 +8,6 @@ def lemmatize(surface_form, language_code):
             if re.search(r'\t' + surface_form + '\t', line):
                 lemma = line.split('\t')[0]  # Each line contains: lemma surface_form details
                 return lemma
-        return 'Not in resource.'
 
 
 def expand_lemma_to_surface_forms(lemma_to_expand, language_code):
@@ -19,11 +18,7 @@ def expand_lemma_to_surface_forms(lemma_to_expand, language_code):
             current_lemma, surface_form, _ = line.split('\t')
             if current_lemma == lemma_to_expand:
                 surface_forms.append(surface_form)
-
-    if len(surface_forms) > 1:
-        return set(surface_forms)
-    else:
-        return 'Not in resource.'
+    return set(surface_forms)
 
 
 def get_related_morphological_forms(word_to_reinflect, language_code='sv'):
@@ -32,14 +27,22 @@ def get_related_morphological_forms(word_to_reinflect, language_code='sv'):
     return expanded_forms
 
 
+def pretty_print(results):
+    if len(results)>0:
+        for item in sorted(results, key=lambda x: len(x)):
+            print(item)
+    else:
+        print('Not in resource.')
+
+
 if __name__ == '__main__':
     input_word = sys.argv[1]
     try:
         language_code = sys.argv[2]
         print('Expanding {0}. Language code is {1}.'.format(input_word, language_code))
         output = get_related_morphological_forms(input_word, language_code)
+        pretty_print(output)
     except IndexError:
         print('Expanding {0}. No language code provided. Default is Swedish (sv).'.format(input_word))
         output = get_related_morphological_forms(input_word)
-    for item in sorted(output, key=lambda x: len(x)):
-        print(item)
+        pretty_print(output)
