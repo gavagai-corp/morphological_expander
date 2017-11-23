41ec1d4bb2caed9753337f70e1f138baae0f0820
Peter Sumbler <peter@gavagai.se>
2017-11-23 13:22:35 +0100

    Initial project containing lemmatizing function.

---
+++ morphological_expander.py
@@ -0,0 +1,12 @@
+import re
+
+
+def lemmatise(surface_form, language):
+    with open('resources/' + language + '.txt') as f:
+        for line in f:
+            if re.match(surface_form, line):
+                lemma = line.split()[0]  # Each line contains: lemma surface_form details
+                return lemma
+
+
+lemmatise('katter', 'sv')
