3e61dd161cd80738087d71424844da6c17df3baf
Peter Sumbler <peter@gavagai.se>
2017-11-23 13:53:39 +0100

    Lemmatizer up and running for Swedish. Blank line removed at start of resource.

--- morphological_expander.py
+++ morphological_expander.py
@@ -2,11 +2,12 @@ import re
 
 
 def lemmatize(surface_form, language):
-    with open('resources/' + language + '.txt') as f:
+    with open('resources/' + language + '.txt', encoding='utf8') as f:
         for line in f:
-            if re.match(surface_form, line):
-                lemma = line.split()[0]  # Each line contains: lemma surface_form details
+            if re.search(r'\t'+surface_form+'\t', line):
+                lemma = line.split('\t')[0]  # Each line contains: lemma surface_form details
                 return lemma
+        return 'Not in resource'
 
 
-lemmatise('katter', 'sv')
+print(lemmatize('abstraktionernas', 'sv'))
