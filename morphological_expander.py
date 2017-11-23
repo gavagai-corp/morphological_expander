c4df20f280b3c49c3a0700c1f568d74b1a2c32e3
Peter Sumbler <peter@gavagai.se>
2017-11-23 14:41:24 +0100

    Re-inflection functionality implemented. Command line arguments enabled.

--- morphological_expander.py
+++ morphological_expander.py
@@ -1,13 +1,43 @@
 import re
+import sys
 
 
-def lemmatize(surface_form, language):
-    with open('resources/' + language + '.txt', encoding='utf8') as f:
+def lemmatize(surface_form, language_code):
+    with open('resources/' + language_code + '.txt', encoding='utf8') as f:
         for line in f:
             if re.search(r'\t'+surface_form+'\t', line):
                 lemma = line.split('\t')[0]  # Each line contains: lemma surface_form details
                 return lemma
-        return 'Not in resource'
+        return 'Not in resource.'
 
 
-print(lemmatize('abstraktionernas', 'sv'))
+def expand_lemma_to_surface_forms(lemma_to_expand, language_code):
+    surface_forms = []
+
+    with open('resources/' + language_code + '.txt', encoding='utf8') as f:
+        for line in f:
+            current_lemma, surface_form, _ = line.split('\t')
+            if current_lemma == lemma_to_expand:
+                surface_forms.append(surface_form)
+
+    if len(surface_forms) > 1:
+        return set(surface_forms)
+    else:
+        return 'Not in resource.'
+
+
+def get_related_morphological_forms(word_to_reinflect, language_code='sv'):
+    lemma = lemmatize(word_to_reinflect,language_code)
+    expanded_forms = expand_lemma_to_surface_forms(lemma,language_code)
+    return expanded_forms
+
+
+if __name__ == '__main__':
+    input_word = sys.argv[1]
+    try:
+        language_code = sys.argv[2]
+        print('Expanding {0}. Language code is {1}.'.format(input_word, language_code))
+        get_related_morphological_forms(input_word, language_code)
+    except ValueError:
+        print('Expanding {0}. No language code provided. Default is Swedish (sv).'.format(input_word))
+        get_related_morphological_forms(input_word)
