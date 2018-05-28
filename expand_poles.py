4a07ba472724a54420280af53b5650dc3d18b3c2
fredols <fredrik.olsson@foi.se>
2018-05-28 10:30:09 +0200

    Command line interface to expand_poles.py. Mark not expanded terms with * at beginning of line in output.

--- expand_poles.py
+++ expand_poles.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from morphological_expander import get_related_morphological_forms
 
 
@@ -7,7 +8,7 @@ def inflect_pole(pole_file, pole_language_code):
     Any existing duplicates are removed in passing."""
     existing_pole_terms = []
 
-    with open(pole_file,mode='r',encoding='utf8') as f:
+    with open(pole_file, mode='r', encoding='utf8') as f:
         for line in f:
             existing_pole_terms.append(line.strip())
 
@@ -17,6 +18,7 @@ def inflect_pole(pole_file, pole_language_code):
     print('{} terms in pole initially.'.format(len_existing_pole_terms))
 
     retrieved_expansions = []
+    not_expanded = []
     failed_terms = []
     count = 0
 
@@ -26,6 +28,8 @@ def inflect_pole(pole_file, pole_language_code):
             reinflections = get_related_morphological_forms(term, pole_language_code)
             if len(reinflections) > 0:
                 retrieved_expansions += reinflections
+            else:
+                not_expanded.append(term)
         except:
             failed_terms.append(term)
             term += ' FAIL'
@@ -33,17 +37,18 @@ def inflect_pole(pole_file, pole_language_code):
         count += 1
         print('Prog:', round(100 * count / len_existing_pole_terms, 2), term)
 
-    terms_to_add = list(set(retrieved_expansions))
-
-    new_expanded_pole = sorted(list(set(existing_pole_terms + terms_to_add)))
+    new_expanded_pole = sorted(list(set(retrieved_expansions)))
 
     print('{} terms in pole after reinflection.'.format(len(new_expanded_pole)))
+    print('{} terms form the original list were not expanded.'.format(len(not_expanded)))
     print(new_expanded_pole)
     print(failed_terms)
 
     with open(pole_file[:-4] + '_exp.txt', mode='w', encoding='utf8') as f:
         for term in new_expanded_pole:
             f.write(term + '\n')
+        for term in not_expanded:
+            f.write("* {}\n".format(term))
 
 
 def inflect_multiple_poles(directory):
@@ -64,3 +69,14 @@ def directory_stats(directory):
             before = count_lines_in_file(directory + '/' + polefile)
             after = count_lines_in_file(directory + '/' + expanded_pole)
             print(polefile, '\n\t\t\t\t', before, '\t\t', after, '\t\t', after - before, '\n')
+
+
+if __name__ == "__main__":
+    try:
+        input_file = sys.argv[1]
+        language = sys.argv[2]
+        inflect_pole(input_file, language)
+    except:
+        print("Usage: python3 expand_poles.py <input_file> <iso639-1 language code>")
+        print("Entries marked with * in the output have not been morphologically expanded "
+              "and require manual attention.")
